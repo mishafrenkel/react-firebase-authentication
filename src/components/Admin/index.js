49d5723ae9a401cd4a70ea42d608e8f0895a3099
Michael Frenkel <michael.frenkel@outlook.com>
2019-11-01 16:47:59 -0700

    add admin and implement user retreivel from Firebase DB

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,69 @@
 import React from 'react';
 
-const Admin = () => (
-  <div>
-    <h1>Admin</h1>
-  </div>
+import { withFirebase } from '../Firebase';
+
+class AdminPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loading: false,
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase.users().on('value', snapshot => {
+      const userObject = snapshot.val();
+
+      const userList = Object.keys(usersObject).map(key => ({
+        ...userObject[key],
+        uid: key,
+      }));
+
+
+      this.setState({
+        users: userList
+        loading: false,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.users().off();
+  }
+  render() {
+    const { users, loading } = this.state;
+
+    return (
+      <div>
+        <h1>Admin</h1>
+
+        {loading && <div>Loading ...</div>}
+        <UserList users={users} />
+      </div>
+    );
+  }
+}
+
+const UserList = ({ }) => (
+  <ul>
+    {users.map(user => (
+      <li key={user.uid}>
+        <span>
+          <strong>ID:</strong> {user.uid}
+        </span>
+        <span>
+          <strong>E-Mail:</strong> {user.email}
+        </span>
+        <span>
+          <strong>Username:</strong> {user.username}
+        </span>
+      </li>
+    ))}
+  </ul>
 );
 
-export default Admin;
+export default withFirebase(AdminPage);
