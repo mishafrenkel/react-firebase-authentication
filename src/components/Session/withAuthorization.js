f0e6deff59da83487381de5f149803609ef80a52
Michael Frenkel <michael.frenkel@outlook.com>
2019-11-01 15:57:33 -0700

    improve render method in withAuthorization to avoid showing protected page before redirect

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
-
+import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
-import { conditionalExpression } from '@babel/types';
 
-const withAuthorization = () => Component => {
-  class withAuthorization extends React.Component {
+
+const withAuthorization = condition => Component => {
+  class WithAuthorization extends React.Component {
     componentDidMount() {
-      this.listener = this.props.firebase.auth.onAuthStateChange(
-        authUser => {
-          if (!condition(authUser)) {
-            this.props.history.push(ROUTES.SIGN_IN);
-          }
-        },
-      );
+      this.listener = firebase.auth.onAuthStateChanged(authUser => {
+        if (!condition(authUser)) {
+          this.props.history.push(ROUTES.SIGN_IN);
+        }
+      });
     }
 
-    componentWillMount() {
+    componentWillUnmount() {
       this.listener();
     }
-
     render() {
-      return <Component {...this.props} />;
+      return (
+        <AuthUserContext.Consumer>
+          {authUser =>
+            condition(authUser) ? <Component {...this.props} /> : null
+          }
+        </AuthUserContext.Consumer>
+      );
     }
   }
-
   return compose(
     withRouter,
     withFirebase,
-  )(withAuthorization);
+  )(WithAuthorization);
 };
+
+export default withAuthorization;
