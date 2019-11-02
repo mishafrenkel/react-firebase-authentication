47fcd4258a14f1352e4e19db4453f7f7bbb67ba9
Michael Frenkel <michael.frenkel@outlook.com>
2019-11-01 19:29:28 -0700

    create withEmailVerification HoC and protect routes

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -72,6 +72,8 @@ class Firebase {
             authUser = {
               uid: authUser.uid,
               email: authUser.email,
+              emailVerified: authUser.emailVerified,
+               providerData: authUser.providerData,
               ...dbUser,
             };
 
---
+++ src/components/Session/withEmailVerification.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AuthUserContext from './context';
+import { withFirebase } from '../Firebase';
+
+const withEmailVerification = Component => {
+  class WithEmailVerification extends React.Component {
+    render() {
+      return (
+        <AuthUserContext.Consumer>
+          {authUser => <Component {...this.props} />}
+        </AuthUserContext.Consumer>
+      );
+    }
+  }
+
+  return withFirebase(WithEmailVerification);
+};
+
+export default withEmailVerification;
